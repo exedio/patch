d62e33f1453bad0b25e059fb30fdd4bab4d46a8d
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-01-24 13:04:27 +0000

    split PatchesTest off PatchTest
    git-svn-id: https://svn.exedio.com/cope/patch/trunk@27649 e7d4fc99-c606-0410-b9bf-843393a9eab7

---
+++ testsrc/com/exedio/cope/patch/PatchesTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2004-2013  exedio GmbH (www.exedio.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.exedio.cope.patch;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import org.junit.Test;
+
+public class PatchesTest
+{
+	@Test public void nullPatch()
+	{
+		final List<SamplePatch> patches = asList(
+			newSamplePatch("other1"),
+			null,
+			newSamplePatch("other2"),
+			newSamplePatch("other3"));
+		try
+		{
+			Patches.byDescending(patches);
+			fail();
+		}
+		catch(final IllegalArgumentException e)
+		{
+			assertEquals(
+					"null at position 1",
+					e.getMessage());
+		}
+	}
+
+	@Test public void nullID()
+	{
+		final List<SamplePatch> patches = asList(
+			newSamplePatch("other1"),
+			newSamplePatch(null),
+			newSamplePatch("other2"),
+			newSamplePatch("other3"));
+		try
+		{
+			Patches.byDescending(patches);
+			fail();
+		}
+		catch(final IllegalArgumentException e)
+		{
+			assertEquals(
+					"illegal id at position 1 " +
+					"with class com.exedio.cope.patch.SamplePatch",
+					e.getMessage());
+			assertEquals(
+					"mandatory violation for CopePatchRun.patch",
+					e.getCause().getMessage());
+		}
+	}
+
+	@Test public void emptyID()
+	{
+		final List<SamplePatch> patches = asList(
+			newSamplePatch("other1"),
+			newSamplePatch(""),
+			newSamplePatch("other2"),
+			newSamplePatch("other3"));
+		try
+		{
+			Patches.byDescending(patches);
+			fail();
+		}
+		catch(final IllegalArgumentException e)
+		{
+			assertEquals(
+					"illegal id at position 1 " +
+					"with class com.exedio.cope.patch.SamplePatch",
+					e.getMessage());
+			assertEquals(
+					"length violation, '' is too short for CopePatchRun.patch, " +
+					"must be at least 1 characters, but was 0.",
+					e.getCause().getMessage());
+		}
+	}
+
+	@Test public void duplicateID()
+	{
+		final List<SamplePatch> patches = asList(
+			newSamplePatch("other1"),
+			newSamplePatch("duplicate"),
+			newSamplePatch("duplicate"),
+			newSamplePatch("other2"),
+			newSamplePatch("other3"));
+		try
+		{
+			Patches.byDescending(patches);
+			fail();
+		}
+		catch(final IllegalArgumentException e)
+		{
+			assertEquals(
+					"duplicate id >duplicate< " +
+					"at position 2 " +
+					"with class com.exedio.cope.patch.SamplePatch",
+					e.getMessage());
+		}
+	}
+
+	private SamplePatch newSamplePatch(final String id)
+	{
+		return new SamplePatch(null, id, true);
+	}
+}
