361eb7a40c73c74c047395d764faa105593ad063
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2018-08-17 18:18:54 +0200

    idea 2018.1.5: inspection suppressed: ConstantConditions

--- testsrc/com/exedio/cope/patch/PatchesTest.java
+++ testsrc/com/exedio/cope/patch/PatchesTest.java
@@ -34,6 +34,7 @@ public class PatchesTest
 	@Test void patchNull()
 	{
 		final PatchesBuilder builder = new PatchesBuilder();
+		//noinspection ConstantConditions OK: testing bad API usage
 		assertFails(() ->
 			builder.insertAtStart(null),
 			NullPointerException.class, "patch");
