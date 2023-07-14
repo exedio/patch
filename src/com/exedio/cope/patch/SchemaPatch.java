339b88db32abe3a0953f25685f343d961e108e8e
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2023-07-14 13:46:59 +0200

    slf4j-api 1.7.25, is the common baseline in current projects
    slf4j source added to project

--- src/com/exedio/cope/patch/SchemaPatch.java
+++ src/com/exedio/cope/patch/SchemaPatch.java
@@ -147,7 +147,7 @@ public abstract class SchemaPatch implements Patch
 			{
 				final String sql = body[position];
 				if(logger.isInfoEnabled())
-					logger.info("{}/{}: {}", new Object[]{position+1, body.length, sql});
+					logger.info("{}/{}: {}", position+1, body.length, sql);
 				if(ctx.supportsMessage())
 					ctx.setMessage("SchemaPatch " + (position+1) + '/' + body.length + ' ' + sql);
 				final long start = nanoTime();
