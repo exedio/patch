2d66c25f616a1ce288b86e8202af71bc4147ac54
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2025-12-17 14:37:56 +0100

    <instrument serialVersionUIDSuffix="capital"

--- src/com/exedio/cope/patch/PatchMutex.java
+++ src/com/exedio/cope/patch/PatchMutex.java
@@ -197,7 +197,7 @@ final class PatchMutex extends Item
 
 	@com.exedio.cope.instrument.Generated
 	@java.io.Serial
-	private static final long serialVersionUID = 1l;
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * The persistent type information for patchMutex.
--- src/com/exedio/cope/patch/PatchRun.java
+++ src/com/exedio/cope/patch/PatchRun.java
@@ -195,7 +195,7 @@ final class PatchRun extends Item
 
 	@com.exedio.cope.instrument.Generated
 	@java.io.Serial
-	private static final long serialVersionUID = 1l;
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * The persistent type information for patchRun.
